bd3998ee94ebdec284ab1cb8f6a2da5b27f2cd87
HEX <615684880@qq.com>
2022-09-11 20:47:46 +0800

    20220911-修改实体类

---
+++ bm-common/src/main/java/com/bm/entity/Role.java
@@ -0,0 +1,55 @@
+package com.bm.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author hex
+ * @since 2022-09-08
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("bm_role")
+@ApiModel(value="Role对象", description="")
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @ApiModelProperty(value = "角色编码")
+    private String code;
+
+    @ApiModelProperty(value = "角色名")
+    private String name;
+
+    @ApiModelProperty(value = "创建者")
+    private String creater;
+
+    @ApiModelProperty(value = "创建时间")
+    private LocalDateTime createDate;
+
+    @ApiModelProperty(value = "更新者")
+    private String updater;
+
+    @ApiModelProperty(value = "更新时间")
+    private LocalDateTime updateDate;
+
+    @ApiModelProperty(value = "备注")
+    private String comments;
+
+
+}
---
+++ bm-common/src/main/java/com/bm/entity/RolePermission.java
@@ -0,0 +1,55 @@
+package com.bm.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author hex
+ * @since 2022-09-08
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@TableName("bm_role_permission")
+@ApiModel(value="RolePermission对象", description="")
+public class RolePermission implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Integer id;
+
+    @ApiModelProperty(value = "角色ID")
+    private Integer roleId;
+
+    @ApiModelProperty(value = "权限ID")
+    private Integer permissionId;
+
+    @ApiModelProperty(value = "创建者")
+    private String creater;
+
+    @ApiModelProperty(value = "创建时间")
+    private LocalDateTime createDate;
+
+    @ApiModelProperty(value = "更新者")
+    private String updater;
+
+    @ApiModelProperty(value = "更新时间")
+    private LocalDateTime updateDate;
+
+    @ApiModelProperty(value = "备注")
+    private String comments;
+
+
+}
